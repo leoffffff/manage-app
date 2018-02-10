6155a1d02dcccb703e60d56de5334d2cbb8eb671
陈硬地 <chenyingdi@chenyingdideMacBook-Pro.local>
2018-02-10 23:20:31 +0800

    Multiple migrations have the same name

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20171228090448) do
+ActiveRecord::Schema.define(:version => 20171228085923) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
